6bf4d2068d41883c4039ffa22c43143363a2127b
Urs Albisser <u.albisser@gmail.com>
2024-01-05 20:02:57 +0100

    update

--- 11_dynamic_inventory/inventory.py
+++ 11_dynamic_inventory/inventory.py
@@ -39,4 +39,16 @@ if __name__ == "__main__":
     if args and args.list:
         print(json.dumps(invventory_data))
     elif args and args.host:
-        print(json.dumps({'_meta': {'hostsvars': {}}}))
+        # Initialize an empty dictionary for the host vars
+        host_vars = {}
+        
+        # Loop through each group in the inventory data
+        for group, group_data in invventory_data.items():
+            if args.host in group_data["hosts"]:
+                host_vars = group_data["vars"]
+                break
+                
+        print(json.dumps({'_meta': {'hostvars': host_vars}}))
+        #print(group) # returns web
+        #print(group_data) # returns {'hosts': ['web_server'], 'vars': {'ansible_ssh_pass': 'passw0rd', 'ansible_ssh_host': '192.168.1.2'}}
+        #print(host_vars)
